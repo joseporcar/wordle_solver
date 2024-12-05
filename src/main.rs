c0a5f682f0b0a5e484c8e379cba149329596db36
joseporcar <joseaporcar@gmail.com>
2024-12-05 09:45:11 -0400

    It works but still too slow

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,16 @@ use wordle_solver::perfect::PerfectWordSet;
 
 fn main() {
     let mut words = PerfectWordSet::new();
-    let mut count: i32 = 0;
+    // let mut count: i32 = 0;
     while let Ok(_) = words.update() {
         
         if words.is_perfect() {
             println!("{}", words.to_string())
         }
-        println!("{}", words.to_string());
-        count += 1;
-        if count % 10000000 == 0 {
-            println!("{}", words.to_string())
-        }
-        if count == 20 {
-            break;
-        }
+        // count += 1;
+        // if count % 100000000 == 0 {
+        //     println!("{}", words.to_string())
+        // }
     }
 }
 
--- src/perfect.rs
+++ src/perfect.rs
@@ -3,44 +3,26 @@ const WORDS: [&[u8; 5]; crate::UNIQUE_WORDS.len()] = crate::UNIQUE_WORDS;
 
 pub struct PerfectWordSet {
     indexes: [usize; OPENER_COUNT],
+    // This is defined on object level for optimization reasons
     buff: [u8; OPENER_COUNT*5],
     count: usize
 }
 impl PerfectWordSet  {
+
     pub fn new() -> Self {
+        // The following is to put it in proper perfect format
+        let mut indexes = [0; OPENER_COUNT];
+        let mut i = 0;
+        while i < OPENER_COUNT - 1 {
+            i += 1;
+            indexes[i] = Self::next_letter(indexes[i-1]);
+        }
         PerfectWordSet {
-            indexes: (0..OPENER_COUNT).collect::<Vec<usize>>().try_into().unwrap(),
+            indexes: indexes,
             buff: [0; OPENER_COUNT*5],
             count: 0
         }
     }
-
-    // 0: abeng
-    // 452: bachs
-    // 1121: caber
-    // 1795: daces
-    // 2283: eagly
-    // 2434: fable
-    // 2911: gable
-    // 3384: habit
-    // 3721: iambs
-    // 3827: jabot
-    // 3972: kades
-    // 4224: labis
-    // 4644: mabes
-    // 5250: nabes
-    // 5509: oaked
-    // 5745: paced
-    // 6491: qadis
-    // 6576: rabic
-    // 7069: sabed
-    // 7956: taber
-    // 8491: ubacs
-    // 8659: vacui
-    // 8855: wacke
-    // 9159: xenia
-    // 9168: yacht
-    // 9296: zacks
     // returns the index corresponding to the next beginning letter. Example if the input is abing, this would skip past abeys and the rest of as straight to bachs
     // Method will only work for hard_coded words Uniques
     fn next_letter(index: usize) -> usize {
@@ -50,26 +32,47 @@ impl PerfectWordSet  {
             452..1121 => 1121,
             1121..1795 => 1795,
             1795..2283 => 2283,
-            2283.. => 0,
+            2283..2434 => 2434,
+            2434..2911 => 2911,
+            2911..3384 => 3384,
+            3384..3721 => 3721,
+            3721..3827 => 3827,
+            3827..3972 => 3972,
+            3972..4224 => 4224,
+            4224..4644 => 4644,
+            4644..5250 => 5250,
+            5250..5509 => 5509,
+            5509..5745 => 5745,
+            5745..6491 => 6491,
+            6491..6576 => 6576,
+            6576..7069 => 7069,
+            7069..7956 => 7956,
+            7956..8491 => 8491,
+            8491..8659 => 8659,
+            8659..8855 => 8855,
+            8855..9159 => 9159,
+            9159..9168 => 9168,
+            9168..9296 => 9296,
+            9296..     => WORDS.len(),
         }
     }
 
     fn update_index_no_first_letter_repeats(&mut self) -> Result<(), ()> {
-        let mut i = OPENER_COUNT - 1;
+        self.count = OPENER_COUNT - 1;
         
         // Check if the index is going to overflow to change the previous digit. 
-        while i > 0 && self.indexes[i] + 1 > WORDS.len() - OPENER_COUNT + i {
-            i -= 1;
+        while self.count > 0 && self.indexes[self.count] + 1 > WORDS.len() - OPENER_COUNT + self.count {
+            self.count -= 1;
         }
 
-        self.indexes[i] += 1;
+        self.indexes[self.count] += 1;
 
         // Resetting indexes after the.. idk how to explain its like going from 99 to 100, this puts the zeroes
-        while i < OPENER_COUNT - 1 {
-            i += 1;
-            self.indexes[i] = Self::next_letter(self.indexes[i-1]);
+        while self.count < OPENER_COUNT - 1 {
+            self.count += 1;
+            self.indexes[self.count] = Self::next_letter(self.indexes[self.count-1]);
         }
-        if self.indexes[0] < WORDS.len() - OPENER_COUNT + i - 1 {
+        if self.indexes[0] < WORDS.len() - OPENER_COUNT + self.count - 1 {
             Ok(())
         }
         else {Err(())}
